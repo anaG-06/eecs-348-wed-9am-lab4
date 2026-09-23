17f5f802d7b8fded8ec2506ff9b289adfe5f133e
Ana <anavirgi2006@gmail.com>
2026-09-23 17:18:06 -0500

    fix float issue

--- task2.c
+++ task2.c
@@ -17,7 +17,7 @@ int main()
     char userConversion[20];
 
     float newTemp;
-    float newTempC; //for use in temperature categorization
+    float newTempC =0; //for use in temperature categorization
     char category[20] = "default";
     char advisory[20] = "default"; 
 
@@ -25,7 +25,8 @@ int main()
     printf("Enter a temperature:\n");
     // scanf("%d", &userTemp);
     fgets(userTemp, 20, stdin);
-    int INTuserTemp = atoi(userTemp);
+    // int INTuserTemp = atoi(userTemp);
+    float INTuserTemp = atof(userTemp); //it is named INT but it is actually a FLOAT
 
     printf("Enter the original scale (C,F, or K): \n");
     // scanf("%c", &userScale);
@@ -37,40 +38,45 @@ int main()
     fgets(userConversion, 20, stdin);
 
     //temperature conversion
-    if (userScale[0] == 'C')
+    if (userScale[0] == 'C') // userScale is C
     {
-        newTempC = INTuserTemp;
-        if (userConversion == 'K') // c to k
+        newTempC = INTuserTemp; //save temp as C for category and advisory
+        if (userConversion[0] == 'K') // c to k
         {
             newTemp = INTuserTemp + 273.15;
         }
-        else if (userConversion == 'F') // c to f
+        else if (userConversion[0] == 'F') // c to f
         {
-            newTemp = (INTuserTemp * (9/5)) + 32;
+            newTemp = (INTuserTemp * (9.0/5.0)) + 32.0;
         }
     } 
-    else if (userScale == 'K')
+    else if (userScale[0] == 'K') // userScale is K
     {
-        if (userConversion == 'C')// k to c
+        if (userConversion[0] == 'C')// k to c
         {
-            newTemp = INTuserTemp - 273.15;
-            newTempC = newTemp;
+            newTemp = INTuserTemp - 273.15; 
+            newTempC = newTemp; //save temp as C for category and advisory
         }
-        else if (userConversion == 'F') // k to f
+        else if (userConversion[0] == 'F') // k to f
         {
-            newTemp = ((INTuserTemp - 273.15) * (9/5)) + 32;
+            newTemp = ((INTuserTemp - 273.15) * (9.0/5.0)) + 32.0;
+
+            //convert to C for later use in category and advisory
+            newTempC = INTuserTemp - 273.15;
         }
     }
-    else if (userScale == 'F')
+    else if (userScale[0] == 'F') // userScale is F
     {
-        if (userConversion == 'C')// f to c
+        if (userConversion[0] == 'C')// f to c
         {
-            newTemp = (INTuserTemp - 32) * (5/9);
-            newTempC = newTemp;
+            newTemp = (INTuserTemp - 32.0) * (5.0/9.0);
+            newTempC = newTemp; //save temp as C for category and advisory
         }
-        else if (userConversion == 'K')// f to k
+        else if (userConversion[0] == 'K')// f to k
         {
-            newTemp = ((INTuserTemp - 32) * (5/9)) + 273.15;
+            newTemp = ((INTuserTemp - 32.0) * (5.0/9.0)) + 273.15;
+            //convert to C for later use in category and advisory
+            newTempC = (INTuserTemp - 32.0) * (5.0/9.0);
         }
     } 
 
@@ -101,15 +107,16 @@ int main()
         strncpy(advisory, "Stay inside", 20);
     }
     
-    if (userConversion == 'C')
+    //output defined by user conversion
+    if (userConversion[0] == 'C')
     {
         printf("Converted temperature: %f C\nTemperature Category: %s\nWeather Advisory: %s\n", newTemp,category,advisory);
     }
-    else if (userConversion == 'F')
+    else if (userConversion[0] == 'F')
     {
         printf("Converted temperature: %f F\nTemperature Category: %s\nWeather Advisory: %s\n", newTemp,category,advisory);
     }
-    else if (userConversion == 'K')
+    else if (userConversion[0] == 'K')
     {
         printf("Converted temperature: %f K\nTemperature Category: %s\nWeather Advisory: %s\n", newTemp,category,advisory);
     }
